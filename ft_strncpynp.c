52a2cfc5377685e73338079122f6a6d611face4d
Gonzalo FERNANDEZ <gofernan@e3r9p17.42.fr>
2018-05-14 20:01:10 +0200

    updated

--- ft_strncpynp.c
+++ ft_strncpynp.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpynp.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gofernan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/14 17:16:14 by gofernan          #+#    #+#             */
+/*   Updated: 2018/05/14 18:05:16 by gofernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 char	*ft_strncpynp(char *dst, const char *src, size_t len)
@@ -7,7 +19,6 @@ char	*ft_strncpynp(char *dst, const char *src, size_t len)
 
 	i = 0;
 	a = len;
-
 	while (i < a)
 	{
 		dst[i] = src[i];
